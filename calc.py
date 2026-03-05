294e8ff4404c391b799e38a3999a4797174b84b8
Bobio <daoaldon@gmail.com>
2026-03-05 01:25:21 -0500

    added more numbers after the decimal point

--- calc.py
+++ calc.py
@@ -1,10 +1,13 @@
 import sys
+from decimal import *
 
 if len(sys.argv) != 2:
     print("usage: python calc.py <digit>")
     sys.exit(1)
 
 max_digit = int(sys.argv[1])
+
+getcontext().prec = max_digit + 10
 
 k = 0
 
@@ -20,15 +23,15 @@ pi = 0
 term = int(max_digit/14)
 
 while k <= term:
-    current_digit = ((-1)**k) * factorial(6*k) * (545140134*k + 13591409)
-    current_digit = current_digit / (factorial(3*k) * (factorial(k)**3) * (640320)**(3*k+(3/2)))
+    current_digit = Decimal(((-1)**k) * factorial(6*k) * (545140134*k + 13591409))
+    current_digit = current_digit / Decimal(factorial(3*k) * Decimal(factorial(k)**3) * Decimal(640320)**(3*k+Decimal(3/2)))
     
     pi = pi + current_digit
     k = k + 1
 
 pi = pi * 12
 
-pi = 1/pi
+pi = Decimal(1)/pi
 
 
 
